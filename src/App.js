fb1eca7d12a9565405f0b29a21da020b9c664597
kaksus15 <kaksus15@gmail.com>
2020-05-26 06:50:11 +0700

    Menambahkan Component Jumbotron pada Halaman Web

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import NavbarComponent from "./Components/NavbarComponent";
+import JumbotronComponent from "./Components/JumbotronComponent";
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <NavbarComponent />
+        <JumbotronComponent />
       </div>
     );
   }
